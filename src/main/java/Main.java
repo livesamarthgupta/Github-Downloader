6230f391a11b0915dc2faed84c90a8415ad36840
Samarth Gupta <livesamarthgupta@gmail.com>
2021-11-30 19:17:07 +0530

    Added color support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_RESET = "\u001B[0m ";
     private static final Helper helper = Helper.getInstance();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter url for the path: ");
+        System.out.println();
+        System.out.print(ANSI_BLUE_BACKGROUND + "Enter URL for the path:" + ANSI_RESET);
         String url = sc.nextLine();
         System.out.println();
-        System.out.print("Enter OAUTH Token (OPTIONAL): ");
+        System.out.print(ANSI_BLUE_BACKGROUND + "Enter Personal Access Token (OPTIONAL):" + ANSI_RESET);
         String token = sc.nextLine();
         System.out.println();
         String userHome = System.getProperty("user.home");
